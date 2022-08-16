bee9ede375b4c797533204f5b56149a71496020b
xihuishawpy <1040310273@qq.com>
2022-08-17 00:50:50 +0800

    add PCA

--- Machine-Learning/2、手推代码/PCA.py
+++ Machine-Learning/2、手推代码/PCA.py
@@ -4,7 +4,7 @@ Version: 1.0
 Autor: xihuishaw
 Date: 2022-08-17 00:23:09
 LastEditors: xihuishaw
-LastEditTime: 2022-08-17 00:33:52
+LastEditTime: 2022-08-17 00:49:53
 '''
 
 
@@ -20,17 +20,33 @@ class PCA:
         self.mean = np.mean(X, axis=0)
         X = X - self.mean
 
+        # 求协方差矩阵，并计算特征值与特征向量
         cov = np.cov(X.T)
         eigenvalues , eigenvectors = np.linalg.eig(cov)
 
+        # 按特征值从大到小，特征向量依次排序
         eigenvectors = eigenvectors.T
-        idxs = np.argsort(eigenvectors)[::-1]
+        idxs = np.argsort(eigenvalues)[::-1]
         eigenvalues = eigenvalues[idxs]
         eigenvectors = eigenvectors[idxs]
 
+        # 取出前n_components个特征向量，组成【主成分】
         self.components = eigenvectors[0:self.n_components]
 
     def transform(self,X):
+        # 原数据均值中心化后，再乘以主成分矩阵实现降维
         X = X-self.mean
         return np.dot(X,self.components.T)
+
+
+if __name__ == '__main__':
+    from sklearn.datasets import load_boston
+    X , y = load_boston(return_X_y=True)
+    
+    pca = PCA(n_components=10)
+    pca.fit(X)
+    print(pca.transform(X))
+
+
+
 
